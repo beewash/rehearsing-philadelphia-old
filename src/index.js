b8f0e09a019176525a287f2c56f3344566ab6b39
Bryan Washington <beewash92@gmail.com>
2021-06-28 12:07:34 -0400

    Update index.js
    - block out persistQueryClient instances

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { persistQueryClient } from 'react-query/persistQueryClient-experimental';
-import { createLocalStoragePersistor } from 'react-query/createLocalStoragePersistor-experimental';
+// import { persistQueryClient } from 'react-query/persistQueryClient-experimental';
+// import { createLocalStoragePersistor } from 'react-query/createLocalStoragePersistor-experimental';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
@@ -19,12 +19,12 @@ const queryClient = new QueryClient({
   },
 });
 
-const localStoragePersistor = createLocalStoragePersistor()
+// const localStoragePersistor = createLocalStoragePersistor()
 
-persistQueryClient({
-  queryClient,
-  persistor: localStoragePersistor,
-})
+// persistQueryClient({
+//   queryClient,
+//   persistor: localStoragePersistor,
+// })
 
 ReactDOM.render(
   <React.StrictMode>
