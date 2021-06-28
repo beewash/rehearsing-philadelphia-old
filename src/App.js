40d5dc204ce5cbb28f073f76a191427cd141f964
Bryan Washington <beewash92@gmail.com>
2021-06-28 10:53:14 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React from 'react';
+import { Route, Switch, Link } from 'react-router-dom' ;
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
+import NotFound from './NotFound'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className={styles.header}>
+        <Link className={styles.headerLink} to="/">
+          Rehearsing Philadelphia
+        </Link>
       </header>
-    </div>
+
+      <main className={styles.main}>
+        <div className={styles.container}>
+          <Switch>
+            <Route component={NotFound} />
+          </Switch>
+        </div>
+      </main>
+    </>
   );
 }
 
---
+++ src/sanity.js
@@ -0,0 +1,14 @@
+// /src/sanity.js
+import SanityClient from '@sanity/client';
+import ImageUrlBuilder from '@sanity/image-url';
+
+export const sanity = SanityClient({
+  projectId: 'w3yk8tjd',
+  dataset: 'production',
+  // this option enables faster responses
+  // but can return stale data at times.
+  // recommended for client-side queries
+  useCdn: true,
+});
+
+export const imageUrlBuilder = ImageUrlBuilder(sanity);
